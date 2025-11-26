ce80088b5cc8060540d14c801e916bd674dcab96
FelipeSantos-boiBigas <felipehsantos113@gmail.com>
2025-11-26 07:21:57 -0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer>
+        <p><strong>Intelectus</strong> é uma palavra em Latim que significa discernimento, compreensão, entendimento e percepção.
+            É a raiz de termos como "<em>intelecto</em>" e "<em>inteligência</em>" e representa a faculdade de
+            compreender, de conceber e de relacionar ideias, sendo um conceito fundamental na filosofia, especialmente
+            na escolástica medieval.</p>
+        <p>© Intelectus — 2025 Site de Estudos (Projeto Fictício para Trabalho Acadêmico de Desenvolvimento Web em
+                HTML5, CSS, JavaScript e PHP)</p>
+    </footer>
