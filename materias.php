27d14622a8611bb840d88c5f9924c1edb699bf13
FelipeSantos-boiBigas <felipehsantos113@gmail.com>
2025-11-26 07:21:42 -0300

    Update materias.php

--- materias.php
+++ materias.php
@@ -68,15 +68,8 @@ include 'conexao.php';
             </table>
         </section>
     </main>
-    <footer>
-        <p><strong>Intelectus</strong> é uma palavra em Latim que significa discernimento, compreensão, entendimento e percepção.
-            É a raiz de termos como "<em>intelecto</em>" e "<em>inteligência</em>" e representa a faculdade de
-            compreender, de conceber e de relacionar ideias, sendo um conceito fundamental na filosofia, especialmente
-            na escolástica medieval.</p>
-        <p>© Intelectus — 2025 Site de Estudos (Projeto Fictício para Trabalho Acadêmico de Desenvolvimento Web em
-                HTML5, CSS, JavaScript e PHP)</p>
-    </footer>
-
+    
+<?php include 'footer.php'; ?>
 <script src="js/tema.js" defer></script>
 </body>
 </html>
